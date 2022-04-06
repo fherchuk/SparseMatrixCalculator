f26d39dcd85fcc1702ecdf9a9dcf3122feab4c95
dpetrou00 <78162272+dpetrou00@users.noreply.github.com>
2022-04-05 23:00:02 -0400

    Added Matrix Multiplication
    -Implemented matrix multiplication (needs debugging)
    -Added below pointers (like next and previous) to facilitate matrix multiplication
    -Added getter and setter functions for certain class data members

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,49 +1,52 @@
 #include "Matrix.h"
 #include <iostream>
 
+//Hi
 
 //Node constructor
-Node::Node(int data, int row, int col, Node* next, Node* previous){
+Node::Node(int data, int row, int col, Node* next, Node* previous) {
     this->data = data;
     this->next = next;
     this->previous = previous;
     this->row = row;
     this->col = col;
+    this->below = nullptr;
 }
 
 //Matrix constructor
-Matrix::Matrix(){
+Matrix::Matrix() {
     this->head = nullptr;
+    this->head_vertical = nullptr;
     this->tail = nullptr;
     this->list_size = 0;
-    this->max_rows = 0;
-    this->max_cols = 0;
+    this->max_row_index = 0;
+    this->max_col_index = 0;
 }
 
 //Getter for number of non-zero matrix elements
-int Matrix::getSize(){
+int Matrix::getSize() {
     return this->list_size;
 }
 
 //Setter for row length
 void Matrix::setMaxRow(int row) {
-    this->max_rows = row;
+    this->max_row_index = row;
 }
 
 //Setter for column length
 void Matrix::setMaxCol(int col) {
-    this->max_cols = col;
+    this->max_col_index = col;
 }
 
 //Function to add an element
-void Matrix::addElement(int data, int row, int col){
+void Matrix::addElement(int data, int row, int col) {
 
     //Set the new 
-    if (row > max_rows) {
+    if (row > max_row_index) {
         setMaxRow(row);
     }
 
-    if (col > max_cols) {
+    if (col > max_col_index) {
         setMaxCol(col);
     }
 
@@ -57,7 +60,7 @@ void Matrix::addElement(int data, int row, int col){
     while (curr->next != nullptr && (curr->row * 10 + curr->col < new_node->row * 10 + new_node->col)) {
         curr = curr->next;
     }
-    if (curr->row*10+curr->col == new_node->row*10+new_node->col) {
+    if (curr->row * 10 + curr->col == new_node->row * 10 + new_node->col) {
         if (curr->previous != nullptr) {
             curr->previous->next = new_node;
         }
@@ -72,7 +75,7 @@ void Matrix::addElement(int data, int row, int col){
             this->tail = new_node;
         }
     }
-    else if (curr->row*10+curr->col >= new_node->row*10+new_node->col) {
+    else if (curr->row * 10 + curr->col >= new_node->row * 10 + new_node->col) {
         if (curr->previous != nullptr) {
             curr->previous->next = new_node;
             new_node->previous = curr->previous;
@@ -87,12 +90,30 @@ void Matrix::addElement(int data, int row, int col){
         this->tail = new_node;
         this->list_size++;
     }
+
+    Node* temp1 = this->head;
+    this->head_vertical = this->head;
+    while (temp1 != nullptr) {
+        Node* temp2 = this->head;
+        while (temp2 != nullptr) {
+            if (temp2->row + temp2->col * 10 > temp1->row + temp1->col * 10 && (temp1->below == nullptr || temp2->row + temp2->col * 10 < temp1->below->row + temp1->below->col * 10)) {
+                temp1->below = temp2;
+            }
+            if (temp2->row + temp2->col * 10 < this->head_vertical->row + this->head_vertical->col * 10) {
+                this->head_vertical = temp2;
+            }
+            temp2 = temp2->next;
+        }
+        temp1 = temp1->next;
+    }
 }
 
 void Matrix::printMatrix() {
     Node* curr = this->head;
-    for (int i = 0; i <= this->max_rows; i++) {
-        for (int j = 0; j <= this->max_cols; j++) {
+    curr = this->head;
+
+    for (int i = 0; i <= this->max_row_index; i++) {
+        for (int j = 0; j <= this->max_col_index; j++) {
             if (curr->row == i && curr->col == j) {
                 std::cout << curr->data << " ";
                 if (curr->next != nullptr) {
@@ -116,7 +137,7 @@ void Matrix::readFile(std::string file_name) {
 
     // Temporary string to hold a single line of the file
     std::string str;
-    
+
     int i = 0;
     int j;
     // Reads all lines in the file, 1 at at time
@@ -138,6 +159,66 @@ void Matrix::readFile(std::string file_name) {
         i++;
     }
 
-    setMaxRow(i-1);
-    setMaxCol(j-1);
+    setMaxRow(i - 1);
+    setMaxCol(j - 1);
+}
+
+int Matrix::getMaxRow() {
+    return this->max_row_index + 1;
+}
+
+int Matrix::getMaxCol() {
+    return this->max_col_index + 1;
+}
+
+Matrix MatrixOp::matrixMultiply(Matrix one, Matrix two) {
+    Matrix result;
+    if (one.max_col_index != two.max_row_index) {
+        std::cout << "Matrix dimensions do not match.";
+        return result;
+    }
+    else {
+        Node* curr_one = one.head;
+        Node* curr_two = two.head_vertical;
+        for (int i = 0; i <= one.max_row_index; i++) {
+            for (int j = 0; j <= two.max_col_index; j++) {
+                if (curr_one == nullptr || curr_two == nullptr) {
+                    break;
+                }
+                Node* curr_one_a = curr_one;
+                int sum = 0;
+                Node* curr_two_a = curr_two;
+                for (int k = 0; k <= one.max_col_index; k++) {
+                    if (curr_one_a != nullptr && curr_two_a != nullptr && curr_one_a->row == i && curr_one_a->col == k && curr_two_a->row == k && curr_two_a->col == j) {
+                        sum += curr_one_a->data * curr_two_a->data;
+                        curr_one_a = curr_one_a->next;
+                        curr_two_a = curr_two_a->below;
+                    }
+                    if (curr_one_a != nullptr && curr_two_a != nullptr) {
+                        if (curr_one_a->col <= k) {
+                            curr_one_a = curr_one_a->next;
+                        }
+                        if (curr_two_a->row <= k) {
+                            curr_two_a = curr_two_a->below;
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+                while (curr_two != nullptr && curr_two->col != j + 1) {
+                    curr_two = curr_two->below;
+                }
+                if (sum > 0) {
+                    result.addElement(sum, i, j);
+                }
+            }
+            while (curr_one != nullptr && curr_one->row != i + 1) {
+                curr_one = curr_one->next;
+            }
+            curr_two = two.head_vertical;
+        }
+    }
+
+    return result;
 }
--- Matrix.h
+++ Matrix.h
@@ -1,44 +1,66 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <cmath>
 
 class Node
 {
-    private:
-        //The data members of Node consist of a piece of data, their row and column, and next and previous pointers.
-        int data;
-        int row;
-        int col;
-        Node* next;
-        Node* previous;
-    public:
-        //The only constructor takes in parameters to fully define all data members of a Node.
-        Node(int data, int row, int col, Node* next, Node* previous);
+private:
+    //The data members of Node consist of a piece of data, their row and column, and next and previous pointers.
+    int data;
+    int row;
+    int col;
+    Node* next;
+    Node* previous;
+    Node* below;
+public:
+    //The only constructor takes in parameters to fully define all data members of a Node.
+    Node(int data, int row, int col, Node* next, Node* previous);
 
     friend class Matrix;
+    friend class MatrixOp;
 };
 
 class Matrix
 {
-    private:
-        //The data members of Matrix (Linked List) are head and tail pointers and a size variable.
-        Node* head;
-        Node* tail;
-        int list_size;
-        int max_rows;
-        int max_cols;
-    public:
-        //The only and default constructor does not take in parameters.
-        Matrix();
+private:
+    //The data members of Matrix (Linked List) are head and tail pointers and a size variable.
+    Node* head;
+    Node* head_vertical;
+    Node* tail;
+    int list_size;
+    int max_row_index;
+    int max_col_index;
 
-        int getSize();
+    //Made these private since they are only needed in the context of the public member functions
+    void setMaxRow(int row);
+    void setMaxCol(int col);
 
-        //Method to add an element to the matrix linked list
-        void addElement(int data, int row, int col);
+public:
+    //The only and default constructor does not take in parameters.
+    Matrix();
 
-        void printMatrix();
-        void readFile(std::string file);
+    //Returns the size of non-zero elements in the spare matrix
+    int getSize();
+    int getMaxRow();
+    int getMaxCol();
 
-        void setMaxRow(int row);
-        void setMaxCol(int col);
+    //Method to add an element to the matrix linked list
+    void addElement(int data, int row, int col);
+
+    //Prints the matrix in a 2d format in output
+    void printMatrix();
+
+    //Reads a text file
+    void readFile(std::string file);
+
+    //Checks to see if two matrices can be multiplied and returns result if possible
+
+    friend class MatrixOp;
+};
+
+class MatrixOp
+{
+public:
+    Matrix matrixMultiply(Matrix one, Matrix two);
 };
