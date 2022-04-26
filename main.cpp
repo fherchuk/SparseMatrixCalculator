72ae967fea817760cd3d4d1442fa35068d2ae5f4
fherchuk <71285964+fherchuk@users.noreply.github.com>
2022-04-26 03:19:35 -0400

    sfml Update
    Added GUI to matrix functions.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
+#include<iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+#include <sstream>
 #include "Matrix.h"
+#include "Node.h"
+#include "Graphics.h"
 
 int main() {
-	Matrix m;
-	m.randomMatrix(8,8,-9,9,5);
-	m.printMatrix();
-	m.writeFile("out.txt");
-	int a;
-	std::cin >> a;
+
+	RenderWindow main1;
+	main1.RenderMain();
+
+		//m.printMatrix();
+		//n.printMatrix();
+
+
+	return 0;
 }
